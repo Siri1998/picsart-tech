b677928791b286b8d2bc1fb791ec6397273be380
Siri <si.grigoryan@roi4cio.com>
2024-06-09 05:14:52 +0400

    add ordering

--- src/components/CustomTable/CustomTable.tsx
+++ src/components/CustomTable/CustomTable.tsx
@@ -14,26 +14,53 @@ import {
 import { DotsIcon } from '../../assets/icons/dots';
 import { ClickOutside, EXCEPT_SELECTORS } from '../ClickOutside';
 import { ITableProps } from './types';
+import { orderBy } from 'lodash';
 
 const CustomTable: React.FC<ITableProps> = ({ columns, data, handleGoDetails }) => {
   const [openMenuIndex, setOpenMenuIndex] = useState<number | null>(null);
+  const [sortedData, setSortedData] = useState(data);
+
+  const [sortConfig, setSortConfig] = useState<{
+    key: string;
+    direction: 'asc' | 'desc';
+  } | null>(null);
 
   const handleMenuClick = (index: number) => {
     setOpenMenuIndex(openMenuIndex === index ? null : index);
   };
 
+  const handleSort = (key: string) => {
+    if (key === 'actions') return;
+
+    let direction: 'asc' | 'desc' = 'asc';
+    if (sortConfig && sortConfig.key === key && sortConfig.direction === 'asc') {
+      direction = 'desc';
+    }
+
+    const sortedArray = orderBy(data, [key], [direction]);
+
+    setSortedData(sortedArray);
+    setSortConfig({ key, direction });
+  };
   return (
     <TableContainer>
       <Table>
         <Thead>
           <Tr>
             {columns.map((column, index) => (
-              <Th key={index}>{column.header}</Th>
+              <Th key={index} onClick={() => handleSort(column.accessor)}>
+                {column.header}
+                {sortConfig?.key === column.accessor
+                  ? sortConfig.direction === 'asc'
+                    ? ' 🔼'
+                    : ' 🔽'
+                  : ''}
+              </Th>
             ))}
           </Tr>
         </Thead>
         <Tbody>
-          {data.map((row, rowIndex) => (
+          {sortedData.map((row, rowIndex) => (
             <Tr key={rowIndex}>
               {columns.map((column, colIndex) => (
                 <Td key={colIndex}>
